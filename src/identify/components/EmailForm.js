d787025588ad79a5bb273916a5fba6a385c434d6
srmward <srmward@gmail.com>
2018-09-30 16:56:12 -0700

    Added Mutation Logic for Sendgrid Newsletter Signup

--- src/identify/components/EmailForm.js
+++ src/identify/components/EmailForm.js
@@ -5,30 +5,59 @@ import {
   labelStyles,
   buttonStyles,
 } from '../../common/styles'
+import gql from 'graphql-tag'
+import { Mutation } from 'react-apollo'
 
+const SUBSCRIBE_USER_TO_NEWSLETTER = gql`
+  mutation subscribe($email: String!) {
+    subscribe(email: $email)
+  }
+`
 export default class EmailForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+    }
+  }
+  handleChange = e => {
+    let newState = {}
+    newState[e.target.name] = e.target.value
+    this.setState(newState)
+  }
+
+  //TODO create snackbar that notifies signup success/failure
   render() {
     return (
-      <form
-        css={formStyles}
-        onSubmit={e => {
-          e.preventDefault()
-          signup({
-            variables: this.state,
-          })
+      <Mutation mutation={SUBSCRIBE_USER_TO_NEWSLETTER}>
+        {(subscribe, { data, error }) => {
+          return (
+            <form
+              css={formStyles}
+              onSubmit={e => {
+                e.preventDefault()
+                subscribe({
+                  variables: {
+                    email: this.state.email,
+                  },
+                })
+              }}
+            >
+              <label css={labelStyles}>email</label>
+              <input
+                css={inputStyles}
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+                required
+              />
+              <button css={buttonStyles} type="submit">
+                email me
+              </button>
+            </form>
+          )
         }}
-      >
-        <label css={labelStyles}>email</label>
-        <input
-          css={inputStyles}
-          name="email"
-          onChange={this.handleChange}
-          required
-        />
-        <button css={buttonStyles} type="submit">
-          email me
-        </button>
-      </form>
+      </Mutation>
     )
   }
 }
