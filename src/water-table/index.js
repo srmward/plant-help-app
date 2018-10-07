aca455bfb38877d04637e51644ff776521ad08bb
srmward <srmward@gmail.com>
2018-10-07 12:25:09 -0700

    Add Image to Water Table

--- src/water-table/index.js
+++ src/water-table/index.js
@@ -4,8 +4,16 @@ import gql from 'graphql-tag'
 import { AuthConsumer } from '../common/contexts/AuthContext'
 import RemoveFromCollectionButton from '../common/ctas/RemoveFromCollectionButton'
 import Cookies from 'js-cookie'
-import { waterTableLayoutStyles, waterTableHeadlineStyles } from './styles'
+import {
+  waterTableLayoutStyles,
+  waterTableHeadlineStyles,
+  waterTableImageStyles,
+  waterTableDataText,
+  waterTableDataContainerStyles,
+} from './styles'
 
+// TODO Handle display of images based on request, build query that allows fetching of user
+// data with image size arg
 export default () => (
   <div>
     <AuthConsumer>
@@ -18,6 +26,10 @@ export default () => (
                   name
                   plants {
                     name
+                    images {
+                      url
+                      size
+                    }
                     exposure
                     repot
                     frequency
@@ -37,11 +49,21 @@ export default () => (
                   {data.users[0].plants.length ? (
                     <div>
                       {data.users[0].plants.map((p, i) => (
-                        <div key={i}>
+                        <div key={i} css={waterTableDataContainerStyles}>
                           <h4>{p.name}</h4>
-                          <p>light exposure: {p.exposure}</p>
-                          <p>water frequency: {p.frequency}</p>
-                          {p.repot && <p>repot: {p.repot}</p>}
+                          <img
+                            css={waterTableImageStyles}
+                            src={p.images[0].url}
+                          />
+                          <p css={waterTableDataText}>
+                            light exposure: {p.exposure}
+                          </p>
+                          <p css={waterTableDataText}>
+                            water frequency: {p.frequency}
+                          </p>
+                          {p.repot && (
+                            <p css={waterTableDataText}>repot: {p.repot}</p>
+                          )}
                           <RemoveFromCollectionButton
                             email={Cookies.get('email')}
                             plantName={p.name}
--- src/water-table/styles.js
+++ src/water-table/styles.js
@@ -1,5 +1,5 @@
 import { css } from 'emotion'
-import { LAYOUT_STYLES } from '../theme'
+import { LAYOUT_STYLES, MEDIA } from '../theme'
 import { headingStyles } from '../common/styles'
 
 export const waterTableHeadlineStyles = css`
@@ -8,4 +8,19 @@ export const waterTableHeadlineStyles = css`
 `
 export const waterTableLayoutStyles = css`
   ${LAYOUT_STYLES};
+`
+export const waterTableImageStyles = css`
+  max-width: 300px;
+`
+
+export const waterTableDataContainerStyles = css`
+  margin-bottom: 4.5rem;
+`
+
+export const waterTableDataText = css`
+  ${MEDIA.TABLET_DOWN} {
+    line-height: 1.4rem;
+    font-size: 0.8rem;
+    display: block;
+  }
 `
