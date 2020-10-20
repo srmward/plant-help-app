1ed94099061d591d421a0c064f903d6182a7b391
srmward <srmward@gmail.com>
2020-10-19 21:37:46 -0700

    New GraphQL URL

--- src/graphql.config.js
+++ src/graphql.config.js
@@ -1,6 +1,6 @@
 import ApolloClient from 'apollo-boost'
 
 export const client = new ApolloClient({
-  uri: 'https://plant-help-graphql-kiyuuwhhzv.now.sh',
+  uri: 'https://plant-help-graphql.uc.r.appspot.com/',
   // uri: 'http://localhost:4000',
 })
